58bb79f398be1e94835b861d8d8a7f579cd43239
Farzin Firouzi <farzineff@gmail.com>
2022-07-25 22:00:45 -0400

    --

--- web_scraping/info_strada_sports_scrape.py
+++ web_scraping/info_strada_sports_scrape.py
@@ -13,8 +13,6 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
 import pandas as pd
-import time
-from pprint import pprint
 
 NUM_OF_ATHLETE_PROFILES_TO_CAPTURE = 20
 INFOSTRADASPORTS_URL = 'http://ipc.infostradasports.com/asp/lib/TheASP.asp?pageid=8903&sportid=514&NOCClubID=-1&Olympic=0&WinterGames=-1&ContinentGeoID=-1'
@@ -82,35 +80,40 @@ def scrape_human_interest_info(bio_table):
 # Script:
 
 
-driver = webdriver.Chrome(ChromeDriverManager().install())
-driver.get(INFOSTRADASPORTS_URL)
-links = driver.find_elements_by_xpath("//a[@href]")
-athlete_profiles = []
-human_interest_infos = []
-competition_highlights_infos = []
+def main():
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    driver.get(INFOSTRADASPORTS_URL)
+    links = driver.find_elements_by_xpath("//a[@href]")
+    athlete_profiles = []
+    human_interest_infos = []
 
-for index, link in enumerate(links):
-    if index == 0:
-        continue
-    else:
-        link.click()
-        html = driver.page_source
-        soup = BeautifulSoup(html, 'html.parser')
-        bio_tables = soup.find_all('table', {'class': 'table_layout1'})
-        athlete_profile_soup = bio_tables[0]
-        human_interest_info_soup = bio_tables[1]
-        athlete_profile = scrape_athlete_profile(athlete_profile_soup)
-        athlete_profiles.append(athlete_profile)
-        human_interest_info = scrape_human_interest_info(human_interest_info_soup)
-        human_interest_infos.append(human_interest_info)
-        driver.back()
-    if NUM_OF_ATHLETE_PROFILES_TO_CAPTURE == 0:
-        continue
-    elif index == NUM_OF_ATHLETE_PROFILES_TO_CAPTURE:
-        break
-athlete_database = get_athlete_database(athlete_profiles)
-df1 = pd.DataFrame(athlete_database)
-athlete_interest_database = get_athlete_database(human_interest_infos)
-df2 = pd.DataFrame(athlete_interest_database)
-df_final = pd.concat([df1, df2], axis=1)
-df_final.to_csv('infostradasports_scrape_data.csv')
+    for index, link in enumerate(links):
+        if index == 0:
+            continue
+        else:
+            link.click()
+            html = driver.page_source
+            soup = BeautifulSoup(html, 'html.parser')
+            bio_tables = soup.find_all('table', {'class': 'table_layout1'})
+            athlete_profile_soup = bio_tables[0]
+            human_interest_info_soup = bio_tables[1]
+            athlete_profile = scrape_athlete_profile(athlete_profile_soup)
+            athlete_profiles.append(athlete_profile)
+            human_interest_info = scrape_human_interest_info(human_interest_info_soup)
+            human_interest_infos.append(human_interest_info)
+            driver.back()
+        if NUM_OF_ATHLETE_PROFILES_TO_CAPTURE == 0:
+            continue
+        elif index == NUM_OF_ATHLETE_PROFILES_TO_CAPTURE:
+            break
+
+    athlete_database = get_athlete_database(athlete_profiles)
+    df1 = pd.DataFrame(athlete_database)
+    athlete_interest_database = get_athlete_database(human_interest_infos)
+    df2 = pd.DataFrame(athlete_interest_database)
+    df_final = pd.concat([df1, df2], axis=1)
+    df_final.to_csv('infostradasports_scrape_data.csv')
+
+
+if __name__ == '__main__':
+    main()
